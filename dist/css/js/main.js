70bdd67eafb77653cbdebb7d9d9fe86d024af917
@TuuliSavolainen <tuuli.savolainen@hotmail.com>
2022-02-01 23:40:00 +0200

    Starting menubar, created main.js
    Menu bar shows when clicked but does not close, transform need to be fixed

---
+++ dist/css/js/main.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+const menuItems = document.querySelector('.menu-items');
+const menuItem = document.querySelectorAll('.menu-item');
+
+let showMenu = false;
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        console.log('moi')
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuItems.classList.add('show');
+        menuItem.forEach(element => element.classList.add('show'));
+   
+        showMenu = true
+    }
+    else {
+        console.log('kukkuluuru')
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuItems.classList.add('show');
+        menuItem.forEach(element => element.classList.add('show'));
+   
+        showMenu = false
+    }
+}
+
+console.log(showMenu);
